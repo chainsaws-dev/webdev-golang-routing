5a061ed9273811c7f42ea2f4d2ba07d79c606dce
Саша Воробьёв <avorobiev@deloitte.ru>
2020-07-01 19:45:34 +0300

    did exercise 02

---
+++ 02/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"text/template"
+)
+
+func main() {
+	sm := http.NewServeMux()
+
+	sm.HandleFunc("/", roothandler)
+	sm.HandleFunc("/dog/", doghandler)
+	sm.HandleFunc("/me/", mehandler)
+
+	http.ListenAndServe(":8080", sm)
+}
+
+func roothandler(rw http.ResponseWriter, req *http.Request) {
+	tmp, err := template.ParseFiles("root.html")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tmp.ExecuteTemplate(rw, "root.html", tmp)
+}
+
+func doghandler(rw http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(rw, "Dog says woof!")
+}
+
+func mehandler(rw http.ResponseWriter, req *http.Request) {
+
+	tmp, err := template.ParseFiles("me.html")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	au := strings.Split(req.RequestURI, "/")
+
+	if len(au) >= 2 {
+		if au[2] != "" {
+			tmp.ExecuteTemplate(rw, "me.html", au[2])
+		} else {
+			tmp.ExecuteTemplate(rw, "me.html", "Anon")
+		}
+	}
+
+}
