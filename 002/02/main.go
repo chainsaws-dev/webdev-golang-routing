acbdadb6beea1aff97c8764db6e7a5e351bc17c8
Саша Воробьёв <avorobiev@deloitte.ru>
2020-07-02 19:53:55 +0300

    Solved 002-02

--- 002/02/main.go
+++ 002/02/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"net"
@@ -28,6 +30,22 @@ func main() {
 func handle(conn net.Conn) {
 	defer conn.Close()
 
+	sc := bufio.NewScanner(conn)
+
+	i := 1
+
+	for sc.Scan() {
+		ln := sc.Text()
+		fmt.Println(i, ln)
+		// Can you answer the question as to why "I see you connected." is never written?
+		//
+		// YES. BECAUSE WE NEVER LEAVE THIS LOOP IT CONTINUES TO EXPECT NEW LINES
+		// WE SHOULD CHECK IF ln IS EMPTY STRING AND BREAK IF IT IS
+		i++
+	}
+
+	//Never gets here because we never stop scanning
+	fmt.Println("Code got here.")
 	io.WriteString(conn, "I see you connected.")
 
 }
