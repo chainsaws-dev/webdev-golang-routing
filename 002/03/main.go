10c41304a3738652ed370c2691b67ba74c6fbaa4
Саша Воробьёв <avorobiev@deloitte.ru>
2020-07-02 19:57:10 +0300

    Solved 002-03

---
+++ 002/03/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Panic(err)
+	}
+	defer lis.Close()
+
+	for {
+		conn, err := lis.Accept()
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	sc := bufio.NewScanner(conn)
+
+	i := 1
+
+	for sc.Scan() {
+		ln := sc.Text()
+		fmt.Println(i, ln)
+
+		if ln == "" {
+			break
+		}
+
+		i++
+	}
+
+	//Never gets here because we never stop scanning
+	fmt.Println("Code got here.")
+	io.WriteString(conn, "I see you connected.")
+
+}
